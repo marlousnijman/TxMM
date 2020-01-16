089df12ccd90bf452b235cc9515db80c923056e5
Marlous Nijman <marlous.nijman@live.nl>
2020-01-16 13:34:54 +0100

    Added correct reply, retweet, and date fields

--- collect_tweets.py
+++ collect_tweets.py
@@ -1,5 +1,6 @@
 from tweepy import OAuthHandler, API
 import unicodecsv
+import datetime
 
 consumer_key=""
 consumer_secret=""
@@ -21,15 +22,25 @@ political_parties = ["VVD",
                      "50pluspartij",
                      "christenunie"]
 
+startDate = datetime.datetime(2019, 1, 1, 0, 0, 0)
+endDate = datetime.datetime(2020, 1, 1, 0, 0, 0)
+
 with open('tweets.csv', mode= 'wb') as tweets_file:
     tweet_writer = unicodecsv.writer(tweets_file, delimiter=',')
     tweet_writer.writerow(["political_party",
                            "tweet_text",
-                           "reply"])
+                           "date",
+                           "reply",
+                           "retweet"])
     for political_party in political_parties:
-        tweets = api.user_timeline(screen_name=political_party, count=10, tweet_mode='extended')
+        tweets = api.user_timeline(screen_name=political_party, tweet_mode='extended')
         for tweet in tweets:
-            reply = tweet.in_reply_to_status_id != ""
-            tweet_writer.writerow([political_party,
-                                   tweet.full_text,
-                                   reply])
+            reply = tweet.in_reply_to_status_id is not None
+            retweet = tweet.full_text.startswith("RT @")
+            correct_date = endDate > tweet.created_at > startDate
+            if correct_date:
+                tweet_writer.writerow([political_party,
+                                       tweet.full_text,
+                                       tweet.created_at,
+                                       reply,
+                                       retweet])
