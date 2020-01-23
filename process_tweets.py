a8d675c591845650c724a345761c6ece7c7fb5db
Marlous Nijman <marlous.nijman@live.nl>
2020-01-23 15:48:20 +0100

    Create word cloud according to word frequencies

--- process_tweets.py
+++ process_tweets.py
@@ -8,6 +8,7 @@ from collections import Counter
 import demoji
 from wordcloud import WordCloud
 
+
 ### Downlaoads
 
 # demoji.download_codes()
@@ -22,6 +23,11 @@ pd.options.mode.chained_assignment = None  # Turn off warnings
 
 
 ### Functions
+
+# Show information about tweet data set
+def show_data_summary(tweets_df):
+    print(tweets_df['political_party'].value_counts())  # Count tweets per party
+
 
 # Load stop words
 def load_stopwords(file_name):
@@ -94,24 +100,24 @@ def compute_topics(tweets_df, stop_words):
     tweets_df = tweets_df[['political_party', 'clean_tweets']].drop_duplicates().reset_index(drop=True)
     tweets_df["bag_of_words"] = tweets_df['clean_tweets'].apply(tokenize)
     tweets_df['word_frequencies'] = tweets_df['bag_of_words'].apply(Counter)
-    tweets_df['topics'] = tweets_df['word_frequencies'].apply(lambda x: x.most_common(30))
-    tweets_df['topics'] = tweets_df['topics'].apply(lambda x: dict(x).keys())
+    tweets_df['topics'] = tweets_df['word_frequencies'].apply(lambda x: x.most_common(50))
+    tweets_df['topics'] = tweets_df['topics'].apply(lambda x: dict(x))
 
     return tweets_df[['political_party', 'topics']]
 
 
 # Generate a word cloud for a string of wowrds
 def generate_word_cloud(words, party):
-    word_cloud = WordCloud().generate(words)
+    word_cloud = WordCloud(background_color="white", colormap='RdPu').generate_from_frequencies(words)
     plt.imshow(word_cloud, interpolation='bilinear')
     plt.axis("off")
+    plt.tight_layout()
     plt.savefig("plots/topics_"+party+".png")
     plt.close()
 
 
 # Create word clouds from the lists of topics discussed for each party
 def topics_to_word_cloud(topic_tweets):
-    topic_tweets['topics'] = topic_tweets['topics'].transform(lambda x: ' '.join(x))
     topic_tweets.apply(lambda x: generate_word_cloud(x.topics, x.political_party),  axis=1)
 
 
@@ -120,10 +126,10 @@ def main():
     tweets = pd.read_csv("tweets.csv", encoding='utf-8')
     stop_words = load_stopwords('stopwords.txt')
     tweets = clean_data(tweets)
+    show_data_summary(tweets)
     tweets = compute_sentiment(tweets)
-    plot_sentiment(tweets)
-    print(tweets['political_party'].value_counts())  # Count tweets per party
     topics = compute_topics(tweets, stop_words)
+    plot_sentiment(tweets)
     topics_to_word_cloud(topics)
 
 
