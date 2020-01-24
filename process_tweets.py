c33d37a56f8e1ed9fafe571e7032e1c83ff2afc9
Marlous Nijman <marlous.nijman@live.nl>
2020-01-24 15:28:55 +0100

    Added custom colormap

--- process_tweets.py
+++ process_tweets.py
@@ -4,6 +4,7 @@ import re
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 from pattern.text.nl import sentiment
 from collections import Counter
 import demoji
@@ -21,6 +22,7 @@ pd.set_option('max_colwidth', 200)
 pd.set_option('display.max_columns', 10)
 pd.set_option('display.max_rows', 10)
 pd.options.mode.chained_assignment = None  # Turn off warnings
+cmap = colors.ListedColormap(['#F9CDAC', '#F3ACA2', '#EE8B97', '#E96A97', '#DB5087','#B8428C', '#973490'])
 
 
 ### Functions
@@ -81,18 +83,6 @@ def compute_sentiment(tweets_df):
     return tweets_df
 
 
-# Plot the average sentiment for each party
-def plot_sentiment(tweets_df):
-    average_polarity = tweets_df.groupby('political_party')['polarity'].mean().sort_values(ascending=False)
-    average_polarity.plot.bar(color="pink")
-    plt.xlabel("Political Party")
-    plt.ylabel("Average Polarity")
-    plt.title("Sentiment of Dutch Political Parties on Twitter")
-    plt.tight_layout()
-    plt.savefig("plots/sentiment.png")
-    plt.close()
-
-
 # Compute topics that political parties tweet about
 def compute_topics(tweets_df):
     tweets_df = tweets_df[['political_party', 'clean_tweets']]
@@ -110,7 +100,7 @@ def compute_topics(tweets_df):
 
 # Generate a word cloud for a string of wowrds
 def generate_word_cloud(words, party):
-    word_cloud = WordCloud(background_color="white", colormap='RdPu').generate_from_frequencies(words)
+    word_cloud = WordCloud(background_color="white", colormap=cmap).generate_from_frequencies(words)
     plt.imshow(word_cloud, interpolation='bilinear')
     plt.axis("off")
     plt.tight_layout()
@@ -145,14 +135,14 @@ def compute_labels(tweets_df):
 def plot_sentiment_topic(tweets_df):
     sentiments = pd.DataFrame({'political_party': tweets_df['political_party'].unique()})
     sentiment_dict = tweets_df.groupby('political_party')['polarity'].mean().to_dict()
-    sentiments['sentiment'] = sentiments['political_party'].map(sentiment_dict)
+    sentiments['General'] = sentiments['political_party'].map(sentiment_dict)
     for filename in os.listdir('topics'):
         topic = filename.replace('.txt', '')
         topic_tweets = tweets_df[tweets_df[topic] == True]
         topic_dict = topic_tweets.groupby('political_party')['polarity'].mean().to_dict()
         sentiments[topic] = sentiments['political_party'].map(topic_dict)
-    sentiments = sentiments.sort_values(by=['sentiment'], ascending=False)
-    sentiments.plot.bar(x="political_party", colormap='PuRd')
+    sentiments = sentiments.sort_values(by=['General'], ascending=False)
+    sentiments.plot.bar(x="political_party", cmap=cmap)
     plt.xlabel("Political Party")
     plt.ylabel("Average Polarity")
     plt.title("Sentiment of Dutch Political Parties on Twitter")
@@ -167,11 +157,10 @@ def main():
     tweets = clean_data(tweets)
     show_data_summary(tweets)
     tweets = compute_sentiment(tweets)
-    # topics = compute_topics(tweets)
-    # plot_sentiment(tweets)
-    # topics_to_word_cloud(topics)
     tweets = compute_labels(tweets)
     plot_sentiment_topic(tweets)
+    topics = compute_topics(tweets)
+    topics_to_word_cloud(topics)
 
 
 # Run main
