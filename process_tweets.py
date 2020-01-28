7066f6bdd42000a3550b045840f12480b66192e0
Marlous Nijman <marlous.nijman@live.nl>
2020-01-28 10:58:07 +0100

    Updated combined sentiments plot and added statistics

--- process_tweets.py
+++ process_tweets.py
@@ -32,6 +32,8 @@ def show_data_summary(tweets_df):
     print(tweets_df['political_party'].value_counts())  # Count tweets per party
     print(tweets_df.groupby('political_party')['retweet'].value_counts())
     print(tweets_df.groupby('political_party')['reply'].value_counts())
+    print("Mean: "+str(tweets_df['polarity'].mean()))
+    print("Standard Deviation: "+str(tweets_df['polarity'].std()))
 
 
 # Load stop words
@@ -85,36 +87,6 @@ def compute_sentiment(tweets_df):
     return tweets_df
 
 
-# Compute topics that political parties tweet about
-def compute_topics(tweets_df):
-    tweets_df = tweets_df[['political_party', 'clean_tweets']]
-    stop_words = load_textfile('stopwords.txt')  # https://eikhart.com/blog/dutch-stopwords-list
-    tweets_df['clean_tweets'] = tweets_df['clean_tweets'].apply(lambda x: remove_stopwords(x, stop_words))
-    tweets_df['clean_tweets'] = tweets_df.groupby('political_party')['clean_tweets'].transform(lambda x: ' '.join(x))
-    tweets_df = tweets_df[['political_party', 'clean_tweets']].drop_duplicates().reset_index(drop=True)
-    tweets_df["bag_of_words"] = tweets_df['clean_tweets'].apply(tokenize)
-    tweets_df['word_frequencies'] = tweets_df['bag_of_words'].apply(Counter)
-    tweets_df['topics'] = tweets_df['word_frequencies'].apply(lambda x: x.most_common(50))
-    tweets_df['topics'] = tweets_df['topics'].apply(lambda x: dict(x))
-
-    return tweets_df[['political_party', 'topics']]
-
-
-# Generate a word cloud for a string of wowrds
-def generate_word_cloud(words, party):
-    word_cloud = WordCloud(background_color="white", colormap=cmap).generate_from_frequencies(words)
-    plt.imshow(word_cloud, interpolation='bilinear')
-    plt.axis("off")
-    plt.tight_layout()
-    plt.savefig("plots/topics_"+party+".png")
-    plt.close()
-
-
-# Create word clouds from the lists of topics discussed for each party
-def topics_to_word_cloud(topic_tweets):
-    topic_tweets.apply(lambda x: generate_word_cloud(x.topics, x.political_party),  axis=1)
-
-
 # Assign a tweet a label if it contains a word from the topic list
 def assign_label(tweet, topic_list):
     if [w for w in topic_list if w in tweet]:
@@ -151,13 +123,45 @@ def compute_sentiment_for_topics(tweets_df):
 # Plot the sentiment of political parties towards certain topics
 def plot_sentiment_topic(tweets_df):
     sentiments = compute_sentiment_for_topics(tweets_df)
-    sentiments.plot.bar(x="political_party", cmap=cmap)
-    plt.xlabel("Political Party")
-    plt.ylabel("Average Polarity")
+    sentiments.plot.barh(x="political_party", cmap=cmap)
+    plt.gca().invert_yaxis()
+    plt.axvline(sentiments['General'].mean(), color='grey', linewidth=0.75, linestyle="--")
+    plt.ylabel("Political Party")
+    plt.xlabel("Average Polarity")
     plt.title("Sentiment of Dutch Political Parties on Twitter")
     plt.tight_layout()
     plt.savefig("plots/sentiments.png")
     plt.close()
+
+
+# Compute topics that political parties tweet about
+def compute_topics(tweets_df):
+    tweets_df = tweets_df[['political_party', 'clean_tweets']]
+    stop_words = load_textfile('stopwords.txt')  # https://eikhart.com/blog/dutch-stopwords-list
+    tweets_df['clean_tweets'] = tweets_df['clean_tweets'].apply(lambda x: remove_stopwords(x, stop_words))
+    tweets_df['clean_tweets'] = tweets_df.groupby('political_party')['clean_tweets'].transform(lambda x: ' '.join(x))
+    tweets_df = tweets_df[['political_party', 'clean_tweets']].drop_duplicates().reset_index(drop=True)
+    tweets_df["bag_of_words"] = tweets_df['clean_tweets'].apply(tokenize)
+    tweets_df['word_frequencies'] = tweets_df['bag_of_words'].apply(Counter)
+    tweets_df['topics'] = tweets_df['word_frequencies'].apply(lambda x: x.most_common(50))
+    tweets_df['topics'] = tweets_df['topics'].apply(lambda x: dict(x))
+
+    return tweets_df[['political_party', 'topics']]
+
+
+# Generate a word cloud for a string of wowrds
+def generate_word_cloud(words, party):
+    word_cloud = WordCloud(background_color="white", colormap=cmap).generate_from_frequencies(words)
+    plt.imshow(word_cloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.tight_layout()
+    plt.savefig("plots/topics_"+party+".png")
+    plt.close()
+
+
+# Create word clouds from the lists of topics discussed for each party
+def topics_to_word_cloud(topic_tweets):
+    topic_tweets.apply(lambda x: generate_word_cloud(x.topics, x.political_party),  axis=1)
 
 
 # Save final dataframe to excel for easy inspection of tweets ordered by polarity
@@ -170,12 +174,12 @@ def save_to_excel(tweets_df, filename):
 def main():
     tweets = pd.read_csv("tweets.csv", encoding='utf-8')
     tweets = clean_data(tweets)
-    show_data_summary(tweets)
     tweets = compute_sentiment(tweets)
     tweets = compute_labels(tweets)
     plot_sentiment_topic(tweets)
     topics = compute_topics(tweets)
     topics_to_word_cloud(topics)
+    show_data_summary(tweets)
     save_to_excel(tweets, "processed_tweets.xlsx")
 
 
